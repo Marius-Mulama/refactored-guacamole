e8e45a1d10dc20037ba5afb8159be8c45ce42bb0
Marius <mulamamarius@gmail.com>
2021-10-31 01:27:41 +0300

    Commited to development l2

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ dotenv.config();
 const reviewsRoute = require("./api/v1/routes/reviews");
 const businessRoute = require("./api/v1/routes/business");
 const accountsRoute = require("./api/v1/routes/accounts");
+const testRoute = require("./api/v1/routes/test");
 
 //Connect to mongoDB
 mongoose.connect("mongodb+srv://Reviews:"+process.env.USER_DB_PASS+"@users.6h2lh.mongodb.net/developmentDB?retryWrites=true&w=majority",{
@@ -25,9 +26,11 @@ mongoose.connect("mongodb+srv://Reviews:"+process.env.USER_DB_PASS+"@users.6h2lh
     console.log("Connected to database");
 });
 
+
 app.use('/api/v1/reviews', reviewsRoute);
 app.use('/api/v1/business', businessRoute);
 app.use('/api/v1/accounts',accountsRoute);
+app.use('/api/v1/test',testRoute);
 
 
 app.use('*',(req,res)=>{
